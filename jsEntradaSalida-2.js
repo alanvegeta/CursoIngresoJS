956988fcf2f909575cb37984fc2d51b1ae71fc71
alanvegeta <40776564+alanvegeta@users.noreply.github.com>
2018-07-02 21:54:41 -0300

    ejercicio 1

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,7 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+ 	var person = prompt("Please enter your name", "Harry Potter");
+	
+}
